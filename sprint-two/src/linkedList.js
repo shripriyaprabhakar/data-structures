d7e556426d5b5a24e08eb57a1f62d746fad6cb25
shripriyaprabhakar <37489016+shripriyaprabhakar@users.noreply.github.com>
2018-06-15 09:46:52 -0700

    finished linked lists

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,13 +4,37 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = new Node(value)
+    if (this.tail === null) {
+      this.tail = node
+      this.head = node
+    } else {
+      this.tail = node
+      this.head.next = this.tail
+    }
   };
 
   list.removeHead = function() {
+    if (this.head !== this.tail) {
+      var headoriginal = this.head.value
+      var pointed = this.head.next
+      this.head = pointed
+      return headoriginal
+    }
+    return this.tail.value 
   };
 
   list.contains = function(target) {
-  };
+    for (var key in list) {
+      if (list[key].value === target){
+        return true;
+     }
+     
+    }
+     return false
+   };
+ 
+ 
 
   return list;
 };
